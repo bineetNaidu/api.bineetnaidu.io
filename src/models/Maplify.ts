771183f67ebeb208c9d80c18e9bd0fb4d64ccbbd
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-28 20:17:56 +0530

    add types and interface for more types definations to models

--- src/models/Maplify.ts
+++ src/models/Maplify.ts
@@ -1,6 +1,29 @@
 import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
 
-const { Schema } = mongoose;
+//? An interface that describe the properties
+//? that are required to create a new MAPLIFY data
+interface MaplifyInterface {
+  lat: number;
+  lng: number;
+  desc: string;
+  city: string;
+}
+
+//? An interface that  desc the props
+//? that a USER model has!
+interface MaplifyModel extends mongoose.Model<MaplifyDoc> {
+  build({ city, desc, lat, lng }: MaplifyInterface): MaplifyDoc;
+}
+
+//? An interface that describe the properties
+//? that a User Document has
+interface MaplifyDoc extends mongoose.Document {
+  lat: number;
+  lng: number;
+  desc: string;
+  city: string;
+}
 
 const MaplifySchema = new Schema({
   lat: Number,
@@ -9,4 +32,13 @@ const MaplifySchema = new Schema({
   city: String,
 });
 
-export default mongoose.model('Maplify', MaplifySchema);
+MaplifySchema.statics.build = (data: MaplifyInterface) => {
+  return new Maplify(data);
+};
+
+const Maplify = mongoose.model<MaplifyDoc, MaplifyModel>(
+  'Maplify',
+  MaplifySchema
+);
+
+export default Maplify;
