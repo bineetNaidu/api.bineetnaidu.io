cb2ccddcc047117fe6ae61cacd2955d53b267d63
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-27 15:14:54 +0530

    🎉 add create , getById, update Routes

--- src/controllers/project.ts
+++ src/controllers/project.ts
@@ -1,10 +1,10 @@
 import { Response, Request } from 'express';
-import Project from '../models/Project';
+import Project, { Status } from '../models/Project';
 
 export const getAllProjects = async (req: Request, res: Response) => {
   let projects;
 
-  if (req.params.featured) {
+  if (req.query.featured) {
     projects = await Project.find({
       featured: true,
     });
@@ -19,4 +19,59 @@ export const getAllProjects = async (req: Request, res: Response) => {
   });
 };
 
-export const getProjectByID = () => null;
+export const getProjectByID = async (req: Request, res: Response) => {
+  const { projectId } = req.params;
+  const project = await Project.findOne({ _id: projectId });
+  if (!project) throw new Error('Project Not Found');
+
+  res.json({
+    data: project,
+    success: true,
+    length: 1,
+  });
+};
+
+export const createProject = async (req: Request, res: Response) => {
+  const {
+    name,
+    url,
+    github,
+    completed,
+    featured,
+    description,
+    status,
+  } = req.body;
+
+  const project = Project.build({
+    name,
+    description,
+    url: url || undefined,
+    github: github || undefined,
+    completed: completed || false,
+    featured: featured || false,
+    status: status || Status.Planing,
+  });
+  await project.save();
+  res.json({
+    data: project,
+    success: true,
+    length: 1,
+    created: true,
+  });
+};
+
+export const updateProject = async (req: Request, res: Response) => {
+  const { projectId } = req.params;
+  const project = await Project.findOne({ _id: projectId });
+  if (!project) throw new Error('Project Not Found');
+
+  await project.updateOne(req.body);
+  await project.save();
+
+  res.json({
+    data: project,
+    success: true,
+    length: 1,
+    updated: true,
+  });
+};
--- src/routes/project.ts
+++ src/routes/project.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express';
-import { getAllProjects } from '../controllers/project';
+import isAccessable from '../middlewares/isAccessable';
+import {
+  getAllProjects,
+  getProjectByID,
+  createProject,
+  updateProject,
+} from '../controllers/project';
 
 const r = Router();
 
 r.get('/', getAllProjects);
+r.post('/', isAccessable, createProject);
+r.get('/:projectId', getProjectByID);
+r.put('/:projectId', isAccessable, updateProject);
 
 export default r;
