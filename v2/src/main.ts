134b60bc7fcf92c209cb1224b2ecb1231ffd1e91
bineetNaidu <bineet.s.naidu@gmail.com>
2021-10-20 21:12:04 +0530

    ❇️ configuring main nest server

--- v2/src/main.ts
+++ v2/src/main.ts
@@ -1,8 +1,35 @@
+import * as helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import {
+  PORT,
+  CONNECT_SRC_URLS,
+  FONTS_SRC_URLS,
+  SCRIPTS_SRC_URLS,
+  STYLES_SRC_URLS,
+} from './shared/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: [],
+          connectSrc: ["'self'", ...CONNECT_SRC_URLS],
+          scriptSrc: ["'unsafe-inline'", "'self'", ...SCRIPTS_SRC_URLS],
+          styleSrc: ["'self'", "'unsafe-inline'", ...STYLES_SRC_URLS],
+          workerSrc: ["'self'", 'blob:'],
+          objectSrc: [],
+          imgSrc: ["'self'", 'blob:', 'data:', 'https://images.unsplash.com/'],
+          fontSrc: ["'self'", ...FONTS_SRC_URLS],
+        },
+      },
+    }),
+  );
+  app.setGlobalPrefix('api');
+
+  await app.listen(PORT);
 }
 bootstrap();
