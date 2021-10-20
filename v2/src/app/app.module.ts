657ad34c3117d3e66ba7fb92be9c0fc7cbf2bdcf
bineetNaidu <bineet.s.naidu@gmail.com>
2021-10-20 21:35:36 +0530

    🌮 add DB module

--- v2/src/app/app.module.ts
+++ v2/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
 import { LinksModule } from 'src/links/links.module';
 
 @Module({
-  imports: [LinksModule],
+  imports: [DatabaseModule, LinksModule],
 })
 export class AppModule {}
