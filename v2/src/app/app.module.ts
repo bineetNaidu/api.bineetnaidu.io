0446c893aaf9f8e5ae6a1251a38c42ceddfb00ec
bineetNaidu <bineet.s.naidu@gmail.com>
2021-10-21 11:24:00 +0530

    🎉 create base Projects Resource

--- v2/src/app/app.module.ts
+++ v2/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
 import { LinksModule } from 'src/links/links.module';
 import { MaplifyModule } from 'src/maplify/maplify.module';
+import { ProjectsModule } from 'src/projects/projects.module';
 
 @Module({
-  imports: [DatabaseModule, LinksModule, MaplifyModule],
+  imports: [DatabaseModule, LinksModule, MaplifyModule, ProjectsModule],
 })
 export class AppModule {}
