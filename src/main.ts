f946d609edb8bdddaa58a287923d7708c6352adc
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-08 17:18:52 +0530

    🐛 fix CORS policy config

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,10 @@ import {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:8080',
+    credentials: true,
+  });
   app.use(
     helmet({
       contentSecurityPolicy: {
