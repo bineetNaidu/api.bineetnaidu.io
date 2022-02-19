c5829bc43393ceb9d771d889e7df596c848e7079
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-19 22:35:02 +0530

    🚀 add GraphQL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,19 @@ import { DatabaseModule } from 'src/database/database.module';
 import { LinksModule } from 'src/links/links.module';
 import { MaplifyModule } from 'src/maplify/maplify.module';
 import { ProjectsModule } from 'src/projects/projects.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [DatabaseModule, LinksModule, MaplifyModule, ProjectsModule],
+  imports: [
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LinksModule,
+    MaplifyModule,
+    ProjectsModule,
+  ],
 })
 export class AppModule {}
