9bf3a1d2b0655c8a6e59546e96529e6a23426aeb
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-20 18:40:14 +0530

    🚀 add Base abstract classes

--- src/shared/BaseDocument.model.ts
+++ src/shared/BaseDocument.model.ts
@@ -2,12 +2,18 @@ import { Field, ID, InterfaceType, GraphQLISODateTime } from '@nestjs/graphql';
 
 @InterfaceType()
 export abstract class BaseDocument {
-  @Field(() => ID)
+  @Field(() => ID, { description: 'The unique identifier of the document' })
   _id: string;
 
-  @Field(() => GraphQLISODateTime, { nullable: true })
+  @Field(() => GraphQLISODateTime, {
+    nullable: true,
+    description: 'The date when the document was created',
+  })
   createdAt: Date;
 
-  @Field(() => GraphQLISODateTime, { nullable: true })
+  @Field(() => GraphQLISODateTime, {
+    nullable: true,
+    description: 'The date when the document was updated',
+  })
   updatedAt: Date;
 }
