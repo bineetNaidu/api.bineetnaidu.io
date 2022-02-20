ba503ade75921c84ee486eacd8c61f3662a9bf5d
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-20 22:27:45 +0530

    🐛 update class validations

--- src/user/dto/login.input.ts
+++ src/user/dto/login.input.ts
@@ -1,4 +1,5 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 @InputType({
   description: 'Login Input DTO',
@@ -7,9 +8,13 @@ export class LoginInput {
   @Field({
     description: "User's email ",
   })
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
   @Field({
     description: "User's password ",
   })
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
