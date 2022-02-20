78caa27eaa4cfa1bdd2990e41f3b3aa06479f11d
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-20 19:28:34 +0530

    🎉 add authGruad

---
+++ src/shared/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { UserDocument } from 'src/user/models/user.model';
+import { USER_MODEL_NAME } from '../constants';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    @InjectModel(USER_MODEL_NAME) private userModel: Model<UserDocument>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const ctx = GqlExecutionContext.create(context);
+    const { req } = ctx.getContext();
+    const token = req.headers['x-access-token'];
+
+    try {
+      if (!token) throw new Error('No token was provided');
+      const jwtPayload = this.jwtService.verify(token);
+      if (jwtPayload) {
+        const authUser = this.userModel.findOne({ _id: jwtPayload.userId });
+        req.user = authUser;
+        return true;
+      }
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+}
