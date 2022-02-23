0fcb41be34771990bdf6eb154c97b0cafc1bd5bf
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-23 15:01:47 +0530

    ❇️ adding auth gaurd to images controller

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -6,19 +6,23 @@ import {
   Get,
   Delete,
   Param,
+  UseGuards,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { AuthGuard } from 'src/shared/guards/auth.guard';
 import { ImagesService } from './images.service';
 
 @Controller('images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
+  @UseGuards(AuthGuard)
   @Get()
   async getAll() {
     return await this.imagesService.getAll();
   }
 
+  @UseGuards(AuthGuard)
   @Post('/upload')
   @UseInterceptors(FileInterceptor('image'))
   async upload(@UploadedFile() image: Express.Multer.File) {
@@ -26,6 +30,7 @@ export class ImagesController {
     return await this.imagesService.upload(image);
   }
 
+  @UseGuards(AuthGuard)
   @Delete('/:filename')
   async delete(@Param('filename') filename: string) {
     return await this.imagesService.delete(filename);
--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -4,14 +4,25 @@ import { ImagesController } from './images.controller';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 import { MulterModule } from '@nestjs/platform-express';
 import { storage } from 'src/cloudinary/cloudinary.provider';
-import { IMAGE_MODEL_NAME } from 'src/shared/constants';
+import {
+  IMAGE_MODEL_NAME,
+  USER_MODEL_NAME,
+  JWT_SECRET,
+} from 'src/shared/constants';
 import { ImageSchema } from './models/image.model';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { UserSchema } from 'src/user/models/user.model';
 
 @Module({
   imports: [
+    JwtModule.register({
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
     MongooseModule.forFeature([
       { name: IMAGE_MODEL_NAME, schema: ImageSchema },
+      { name: USER_MODEL_NAME, schema: UserSchema },
     ]),
     CloudinaryModule,
     MulterModule.register({
