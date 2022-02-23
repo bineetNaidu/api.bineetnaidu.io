127177194dea97c0f15a1e878a7632b197964f76
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-23 15:18:45 +0530

    ⚜️ update type defs

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from '@nestjs/common';
 import { User } from '../user/models/user.model';
 
+export type ApiRequestType = Request & {
+  user: User | undefined;
+};
 export interface MyCtx {
-  req: Request & {
-    user: User | undefined;
-  };
+  req: ApiRequestType;
   res: Response;
 }
