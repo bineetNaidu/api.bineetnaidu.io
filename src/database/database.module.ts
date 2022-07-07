708df0646fdbe570a5aedaf5f49cc6b0d4f17f42
bineetNaidu <bineet.s.naidu@gmail.com>
2022-07-07 13:42:14 +0530

    feat: exposing database connection to services

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MONGO_URI } from '../shared/constants';
+import { MONGO_TEST_URI } from '../shared/constants';
+import { DatabaseService } from './database.service';
 
 @Module({
-  imports: [MongooseModule.forRoot(MONGO_URI)],
+  imports: [MongooseModule.forRoot(MONGO_TEST_URI)],
+  providers: [DatabaseService],
+  exports: [DatabaseService],
 })
 export class DatabaseModule {}
